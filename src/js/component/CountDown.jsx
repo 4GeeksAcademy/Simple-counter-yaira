2e7f0dba64413ff0ae939acc3ea1f1bb02ec580d
Yvv1214 <39809411+Yvv1214@users.noreply.github.com>
2023-06-26 22:34:38 +0000

    3

--- src/js/component/CountDown.jsx
+++ src/js/component/CountDown.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
+let timer 
 const CountDown = (props) => {
+    const [countDown, setCountDown] = useState('')
+    let [counter,setCounter] = useState(0)
+
+    const start = () => {
+        
+        console.log(counter);
+             timer = setInterval(() => {
+                    console.log(counter);
+                    setCounter(counter--)
+                    if (counter < 0)
+                    {
+                      clearInterval(timer);
+                      return;
+                    }
+                },1000);
+    }
+    console.log(countDown);
+
+    //  stop.addEventListener('click' , () =>{
+    //      clearInterval(interval);
+    //  })
+    const reset = (target) => {
+        
+          setCounter(0)
+        //   target.value = 0
+        //   e.target.reset();
+          console.log(timer)
+          clearInterval(timer)
+          
+    }
+
     return(
     <div className="bg-light w-25 mx-auto mt-3 rounded p-3">
         <p className="text-center">Count Down Timer</p>
-        <div className="text-center mb-2">0000</div>
-        <input className="mb-2 mx-0" type="text" name="countDown" id="myInput"></input>
+        <div className="text-center mb-2">{counter}</div>
+        <div className="d-flex justify-content-center">
+        <input className="mb-2 mx-0 d-flex align-center" type="text" name="countDown" id="myInput" onChange={(e)=>setCounter(e.target.value)}></input>
+        </div>
         <div className="d-flex align-items-start flex-row justify-content-center">    
             <button className="btn btn-success me-2" type="button" name="countDown" onClick={start}>Start</button>
             <button className="btn btn-danger me-2" type="button" name="countDown" onClick={stop}>Stop</button>
